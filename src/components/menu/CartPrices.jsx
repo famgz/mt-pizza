43803df4ed72643e15fd4b5c8812f23fcc3dd009
famgz <famgz@proton.me>
2024-01-04 18:49:00 -0300

    refactor: cart price as component

---
+++ src/components/menu/CartPrices.jsx
@@ -0,0 +1,14 @@
+export default function CartPrices({subTotal, deliveryFee, total}) {
+  return (
+    <div className='flex justify-end items-center gap-4 py-2'>
+      <div className='text-gray-500 text-lg'>
+        Subtotal: <br />
+        Delivery: <br />
+        Total:
+      </div>
+      <span className='text-lg font-semibold pl-2 text-right'>
+        ${subTotal} <br />${deliveryFee} <br />${total} <br />
+      </span>
+    </div>
+  );
+}
